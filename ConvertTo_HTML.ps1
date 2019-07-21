4b0031b450e75e00489834d74244faf20d2e7cc4
LiquidMovement <53025867+LiquidMovement@users.noreply.github.com>
2019-07-20 19:44:09 -0500

    Add files via upload
    Get-Process, convert to HTML

---
+++ ConvertTo_HTML.ps1
@@ -0,0 +1,3 @@
+﻿$fragment1 = Get-Process -Name GoogleDriveFS | Select-Object Name,Id,Path | ConvertTo-Html -Fragment -PreContent "<h2>Processes</h2>" | Out-String
+$fragment2 = Get-SysInfo -ComputerName localhost | ConvertTo-Html -Fragment -PreContent "<h2>SysInfo</h2>" | Out-String
+ConvertTo-Html -Body "<h1>report</h1>",$fragment1,$fragment2 | Out-File -FilePath "C:\temp\Report.html"
